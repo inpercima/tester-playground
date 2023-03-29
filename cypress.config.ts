6a2baee3ebe235ee68713705b1af1fc3058b8651
Marcel Jänicke <inpercima@gmail.com>
2023-03-29 13:11:18 +0200

    Update @angular/cli to 15.2.4

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   
   e2e: {
-    'baseUrl': 'http://localhost:4200',
-    supportFile: false
+    'baseUrl': 'http://localhost:4200'
   },
   
   
