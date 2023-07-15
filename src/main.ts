9f62563e0022cb2fb671017935cdd3c86308c8d3
inpercima <inpercima@gmail.com>
2023-07-15 16:39:52 +0200

    Add app.config.ts and tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
-import { OverlayModule } from '@angular/cdk/overlay';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, FormsModule, MatToolbarModule, OverlayModule), provideAnimations(), provideHttpClient()],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
